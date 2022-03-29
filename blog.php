3b79032c66d08b552ccf0b12700a6c0006e26a2e
jawadbaig8 <jawadbaig8@gmail.com>
2022-03-29 23:18:37 +0500

    blog added

---
+++ blog.php
@@ -0,0 +1,235 @@
+<?php
+include('header.php');
+include('navbar.php');
+?>
+<!-- Breadcrumb Section Begin -->
+<div class="breacrumb-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb-text">
+                    <a href="index.php"><i class="fa fa-home"></i> Home</a>
+                    <span>Privacy Policy</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb Section Begin -->
+
+<!-- Contact Section Begin -->
+<section class="contact-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-heading" style="background: #f0a302">
+                        <h4 class="h4 text-center">KON JAE</h4>
+                        <h6 class="h6 text-center">Privacy Policy for Cybmerce Tech Private Limited.</h6>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="h5 text-center">Policy</h5>
+                        <hr>
+                        <p>At KON JAE, accessible from https://konjae.com/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by KON JAE and how we use it.</p>
+                        <br>
+                        <br>
+                        <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.
+                        </p>
+                        <br>
+                        <br>
+                        <p>
+                            This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collected on KON JAE. This policy is not applicable to any information collected offline or via channels other than this website.
+                        </p>
+                        <br>
+                        <br>
+                        <h5 class="h5 text-center">Consent.</h5>
+                        <hr>
+                        <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.
+                        </p>
+                        <br>
+                        <br>
+                        <h5 class="h5 text-center">Information we collect</h5>
+                        <hr>
+                        <p>
+                            The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.
+                            <br>
+                            <br>
+
+                            If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.
+                            <br>
+                            <br>
+
+                            When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.
+
+                        </p>
+                        <br>
+                        <br>
+                        <h5 class="h5 text-center">How we use your information</h5>
+                        <hr>
+                        <p>We use the information we collect in various ways, including to:</p>
+                        <br>
+                        <br>
+                        <ul style="list-style-type:disc;margin:  0 0 0 70px">
+                            <li>Provide, operate, and maintain our website</li>
+                            <li>Improve, personalise, and expand our website</li>
+                            <li>Understand and analyse how you use our website</li>
+                            <li>Develop new products, services, features, and functionality</li>
+                            <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
+                            <li>Send you emails</li>
+                            <li>Find and prevent fraud
+                            </li>
+                        </ul>
+                        <br>
+                        <br>
+                        <h5 class="h5 text-center">Log Files</h5>
+                        <hr>
+                        <p>KON JAE follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this as part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analysing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+                        </p>
+                        <br>
+                        <br>
+                        <h5 class="h5 text-center">Cookies and Web Beacons</h5>
+                        <hr>
+                        <p>Like any other website, KON JAE uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimise the users' experience by customising our web page content based on visitors' browser type and/or other information.
+                            <br>
+                            <br>
+                            For more general information on cookies, please read the Cookies article on different authentic websites.
+                        </p>
+                        <br>
+                        <br>
+                        <h5 class="h5 text-center">Advertising Partners Privacy Policies</h5>
+                        <hr>
+                        <p>You may consult this list to find the Privacy Policy for each of the advertising partners of KON JAE.
+                            <br>
+                            <br>
+                            Third-party ad servers or ad networks use technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on KON JAE, which are sent directly to users' browsers. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalise the advertising content that you see on websites that you visit.
+                            <br>
+                            <br>
+                            Note that KON JAE has no access to or control over these cookies that are used by third-party advertisers.
+                        </p>
+                        <br>
+                        <br>
+                        <h5 class="h5 text-center">Third Party Privacy Policies
+                        </h5>
+                        <hr><!-- comment -->
+                        <p>KON JAE's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practises and instructions about how to opt-out of certain options.
+                            <br>
+                            <br>
+                            You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.
+
+                        </p>
+                        <br>
+                        <br>
+                        <h5 class="h5 text-center">CCPA Privacy Rights (Do Not Sell My Personal Information)</h5>
+                        <hr><!-- comment -->
+                        <p>
+                            Under the CCPA, among other rights, California consumers have the right to:
+                            <br>
+                            <br>
+                            Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.
+                            <br>
+                            <br>
+                            Request that a business delete any personal data about the consumer that a business has collected.
+                            <br>
+                            <br>
+                            Request that a business that sells a consumer's personal data, not sell the consumer's personal data.
+                            <br>
+                            <br>
+                            If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+
+                        </p>
+                        <br>
+                        <br>
+                        <h5 class="h5 text-center">GDPR Data Protection Rights</h5>
+                        <hr><!-- comment -->
+                        <p>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:
+                            <br>
+                            <br>
+                            The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.
+                            <br>
+                            <br>
+
+                            The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.
+                            <br>
+                            <br>
+
+                            The right to erasure – You have the right to request that we erase your personal data, under certain conditions.
+                            <br>
+                            <br>
+
+                            The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.
+                            <br>
+                            <br>
+
+                            The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.
+                            <br>
+                            <br>
+
+                            The right to data portability – You have the right to request that we transfer the data that we have collected to another organisation, or directly to you, under certain conditions.
+                            <br>
+                            <br>
+
+                            If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+                        </p>
+                        <br>
+                        <br>
+                        <h5 class="h5 text-center">Children's Information</h5>
+                        <hr><!-- comment -->
+                        <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
+                            <br> 
+                            <br> 
+                            KON JAE does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
+                        </p>
+                    </div>
+                </div>
+                <br>
+                
+            </div>
+        </div>
+        <style>
+            .company_profile a:hover{
+                color: blue;
+            }
+        </style>
+        <!-- <div class="row">
+            <div class="col-lg-5">
+                <div class="card">
+                <div class="card-body">
+                <h3>KON JAE -- AN EMERGING B2B TALENT OF THE PAKISTAN TEXTILE INDUSTRY </h3>
+                <p>
+                KON JAE is a project of ‘Cybmerce Tech Pvt. Ltd’ is a registered company based in Islamabad. ‘KON JAE – Trust, Empower, Future’ an emerging talent of Pakistan in the textile industry and a pioneer in introducing the Business-to-Business (B2B) model – an Online Marketplace of the textile sector in Pakistan. 
+                </p>
+                </div>
+                
+                </div>
+                <img src="img/about_us_two.svg" height="200" alt="">
+
+                <div class="contact-widget">
+                </div>
+            </div>
+            <div class="col-lg-6 offset-lg-1">
+                <div class="contact-form">
+                    <div class="leave-comment">
+                        <img src="img/about_us.svg" height="200" alt="">
+                       <div class="card">
+                        <div class="card-body">
+                        <h4>Objectives:</h4>
+                       <ul class="list-group">
+                       <li class="list-group-item list-group-item-action">To introduce and build capacity for e-business models to the textile industry of Pakistan</li>	 
+                       <li class="list-group-item list-group-item-action">To grow Pakistan Textile Industry with the online B2B model</li>	
+                       <li class="list-group-item list-group-item-action">To elevate contribution of Pakistan Textile Industry in global textile economy</li>	
+                       <li class="list-group-item list-group-item-action">To ensure quality pool for textile merchants, manufacturers, wholesalers, and customers while operating with B2B standards </li>	
+                       </ul>
+
+                        </div>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+    </div>
+</section>
+<!-- Contact Section End -->
+
+<?php
+include('footer.php');
+?>
